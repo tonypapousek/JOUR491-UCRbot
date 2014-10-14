c5a7a69adb50d3cb97f1d98437a7cf38b7bc5eae
Tony Papousek <tony@papousek.org>
2014-10-14 13:25:55 -0500

    Initial city support

--- ucr_bot.py
+++ ucr_bot.py
@@ -11,8 +11,8 @@ import csv, string, datetime
 def percent_change(old_value, new_value):
     return ((new_value - old_value) / abs(old_value)) * 100
 
-def crime_rate(state_stats):
-    return (float(state_stats[1]) / float(state_stats[2])) * 100000
+def crime_rate(crime_count, population_count):
+    return (float(crime_count) / float(population_count)) * 100000
 
 def state_total(list_name, state_name, crime_selector, population_selector):
     # Initialize counters
@@ -23,11 +23,19 @@ def state_total(list_name, state_name, crime_selector, population_selector):
     for current_city in list_name:
         selected_state = current_city[0]
         if state_name == selected_state and current_city[crime_selector]:
-        #    print "%s, %s" % (current_city[2], state_name)
             crime_counter += int(current_city[crime_selector])
             population_counter += int(current_city[population_selector])
             city_counter += 1
     return [city_counter, crime_counter, population_counter]
+
+def print_cities(list_name, state_name, number_of_cities, state_crime_rate):
+    for current_city in list_name:
+        selected_state = current_city[0]
+        if state_name == selected_state and current_city[6]:
+            city_rate_2012 = crime_rate(current_city[6], current_city[3])
+          #  print city_rate_2012
+            print "In 2012, there were %.2f counts of forcible rapes per 100,000 people in %s, %s." % (city_rate_2012, current_city[2], selected_state)
+    return
 
 # Read UCRdata.csv and put it in its own list
 ucr_reader = csv.reader(open("UCRdata.csv", "rU"), dialect=csv.excel)
@@ -60,10 +68,11 @@ for state in state_list:
         state_stats_2009 = state_total(city_list, selected_state, 37, 34)
 
         # calculate crime_rate for 2009 - 2012
-        crime_rate_2012 = crime_rate(state_stats_2012)
-        crime_rate_2011 = crime_rate(state_stats_2011)
-        crime_rate_2010 = crime_rate(state_stats_2010)
-        crime_rate_2009 = crime_rate(state_stats_2009)
+        crime_rate_2012 = crime_rate(state_stats_2012[1], state_stats_2012[2])
+        crime_rate_2011 = crime_rate(state_stats_2011[1], state_stats_2011[2])
+        crime_rate_2010 = crime_rate(state_stats_2010[1], state_stats_2010[2])
+        crime_rate_2009 = crime_rate(state_stats_2009[1], state_stats_2009[2])
+
 
         # calculate change is crime rate between 2012 and 2011
         crime_rate_change = percent_change(crime_rate_2011, crime_rate_2012)
@@ -72,11 +81,12 @@ for state in state_list:
 
         # Determine rise, fall, or no change in crime
         if crime_rate_change > 0:
-            print "This is a %.2f percent increase from the rate of %.2f per 100,000 in 2011." % (abs(crime_rate_change),crime_rate_2011)
+            print "This is a %.2f percent increase from the rate of %.2f per 100,000 in 2011.\n" % (abs(crime_rate_change),crime_rate_2011)
         elif crime_rate_change < 0:
-            print "This is a %.2f percent decrease from the rate of %.2f per 100,000 in 2011." % (abs(crime_rate_change),crime_rate_2011)
+            print "This is a %.2f percent decrease from the rate of %.2f per 100,000 in 2011.\n" % (abs(crime_rate_change),crime_rate_2011)
         else:
-            print "The 2011 rate is the same."
+            print "The 2011 rate is the same.\n"
+        print_cities(city_list, selected_state, state_stats_2012[0], crime_rate_2012)
 
     else:
         print "Huh? Program error.\n You done messed up A-A-Ron!"
